5eeba608bfe584764d0a25d0965db7be37276340
quilletlena2009-ux <quillet.lena2009@gmail.com>
2026-02-23 16:40:35 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ class MyBot(commands.Bot):
                 {"msg": "Le soleil brille sur Poudlard! ☀️", "img": "https://cdn.discordapp.com/attachments/1362231762197156264/1474885740683989012/image.png?ex=699d7422&is=699c22a2&hm=c4585412a1b49b911d96ebece9f3f4e200c6cb2ac94fb4562f5d11519f440ebf"},
                 {"msg": "Il y a pas mal de vent aujourd'hui... 💨", "img": "https://media.discordapp.net/attachments/1362231762197156264/1474885907512295464/image.png?ex=699d744a&is=699c22ca&hm=874ff357ad5b00fbd05487ad75db3c661aa936daecc2ec5b4f37078e411acbab&=&format=webp&quality=lossless&width=573&height=801"},
                 {"msg": "Le ciel d'Ecosse est très nuageux. ☁️", "img": "https://media.discordapp.net/attachments/1362231762197156264/1474886005239840890/image.png?ex=699d7461&is=699c22e1&hm=d56a23f58df3385cc7fadd227da5849c3c38ac3d35f8ef3e7858dcb87bb94987&=&format=webp&quality=lossless&width=573&height=801"},
-                {"msg": "Prévoyez le parapluie, il pleut sur le château. 🌧️", "https://media.discordapp.net/attachments/1362231762197156264/1474885824188383294/image.png?ex=699d7436&is=699c22b6&hm=43e00f1c8cdc2817e6138642befc61d1d235786a2ef623e54581a63a2d970af2&=&format=webp&quality=lossless&width=570&height=800"},
+                {"msg": "Prévoyez le parapluie, il pleut sur le château. 🌧️", "img":
+"https://media.discordapp.net/attachments/1362231762197156264/1474885824188383294/image.png?ex=699d7436&is=699c22b6&hm=43e00f1c8cdc2817e6138642befc61d1d235786a2ef623e54581a63a2d970af2&=&format=webp&quality=lossless&width=570&height=800"},
                 {"msg": "ALERTE ORAGE ! Restez à l'abri. ⚡", "img": "https://media.discordapp.net/attachments/1362231762197156264/1474886156515803247/image.png?ex=699d7485&is=699c2305&hm=18c84f2de7f18e0383be6dae4a618540eeabfcf1847c8c0889bf0d82313348f3&=&format=webp&quality=lossless&width=575&height=800"}
             ]
 
