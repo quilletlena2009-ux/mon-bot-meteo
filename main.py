1b3918ce5a0b6ea50ce021c8f8b2ab4be34e8cd8
quilletlena2009-ux <quillet.lena2009@gmail.com>
2026-02-23 18:42:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 from flask import Flask
 from threading import Thread
 
-# --- SERVEUR ---
+# --- SERVEUR POUR RENDER ---
 app = Flask('')
 @app.route('/')
 def home(): return "Bot en ligne !"
@@ -17,7 +17,7 @@ def keep_alive():
     t = Thread(target=run)
     t.start()
 
-# --- BOT ---
+# --- BOT DISCORD ---
 TOKEN = os.getenv('TOKEN')
 CHANNEL_ID = 1422958632395341875
 
@@ -34,19 +34,19 @@ class MyBot(commands.Bot):
     async def weather_task(self):
         channel = self.get_channel(CHANNEL_ID)
         if channel:
-            msg, img = self.get_meteo()
+            msg, img = self.get_meteo_info()
             embed = discord.Embed(title="Météo du jour", description=msg, color=0x3498db, timestamp=datetime.datetime.now())
             embed.set_image(url=img)
             await channel.send(embed=embed)
 
     @commands.command(name="meteo")
     async def meteo(self, ctx):
-        msg, img = self.get_meteo()
+        msg, img = self.get_meteo_info()
         embed = discord.Embed(title="Météo demandée", description=msg, color=0x3498db)
         embed.set_image(url=img)
         await ctx.send(embed=embed)
 
-    def get_meteo(self):
+    def get_meteo_info(self):
         options = [
             ("Le soleil brille sur Poudlard! ☀️", "https://cdn.discordapp.com/attachments/1362231762197156264/1474885740683989012/image.png?ex=699d7422&is=699c22a2&hm=c4585412a1b49b911d96ebece9f3f4e200c6cb2ac94fb4562f5d11519f440ebf"),
             ("Il y a pas mal de vent aujourd'hui... 💨", "https://media.discordapp.net/attachments/1362231762197156264/1474885907512295464/image.png?ex=699d744a&is=699c22ca&hm=874ff357ad5b00fbd05487ad75db3c661aa936daecc2ec5b4f37078e411acbab"),
